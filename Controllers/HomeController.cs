f41779333d922dd7a99e30788947eefc9632c164
Nick Sturch-Flint <nick.rulez@live.com>
2020-11-21 17:54:06 -0500

    Added CSS and pages, logic to follow

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿//using LabAssignment4_NETD.Models;
+﻿using LabAssignment4_NETD.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,12 +11,32 @@ namespace LabAssignment4_NETD.Controllers
 {
     public class HomeController : Controller
     {
-
+        List<Textbook> textbookList = new List<Textbook>();
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Appraise()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult Appraise(Textbook text)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Message"] = text.ToString();
+                ViewData["Appraisal"] = "";
+
+                return View("Appraised", text);
+            }
+            else
+            {
+                return View("Bad");
+            }
+        }
     }
 }
---
+++ Models/Textbook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabAssignment4_NETD.Models
+{
+    public class Textbook
+    {
+        public string title { get; set; }
+        public string isbn { get; set; }
+        public double version { get; set; }
+        public double price { get; set; }
+        public string condition { get; set; }
+
+        //No Parameter (Default) Constructor
+        public Textbook() {}
+
+        //Parameterized Constructor
+        public Textbook(string title, string isbn, double version, double price, string condition)
+        {
+            this.title = title;
+            this.isbn = isbn;
+            this.version = version;
+            this.price = price;
+            this.condition = condition;
+        }
+
+        //ToString Override
+        public override string ToString()
+        {
+            string output;
+            output = "=========================================" + Environment.NewLine;
+            output += "Textbook Title    : " + this.title + Environment.NewLine;
+            output += "Textbook ISBN     : " + this.isbn + Environment.NewLine;
+            output += "Textbook Version  : " + this.version.ToString() + Environment.NewLine;
+            output += "Textbook Price    : " + this.price.ToString() + Environment.NewLine;
+            output += "Textbook Condition: " + this.condition + Environment.NewLine;
+            output += "=========================================";
+            return output;
+        }
+
+
+    }
+}
